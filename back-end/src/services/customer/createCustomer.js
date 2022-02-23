2f61919c0bec50c07a2d1c66f1a80f0c0df3bb42
Murilo Ribeiro <murilorsv14@gmail.com>
2022-02-23 16:39:04 -0300

    feat: transforma password em hash MD5 e retira consoles.log

--- back-end/src/services/customer/createCustomer.js
+++ back-end/src/services/customer/createCustomer.js
@@ -1,5 +1,6 @@
 const Joi = require('joi');
 const { StatusCodes } = require('http-status-codes');
+const crypto = require('crypto');
 const { customizeError } = require('../../utils');
 const { User } = require('../../database/models');
 const { generateJWT } = require('../../utils');
@@ -26,15 +27,15 @@ const validateCustomer = async ({ name, email, password, role }) => {
 
 const createCustomer = async (customer) => {
   const { name, email, password, role } = customer;
-  console.log(customer);
 
   await validateCustomer({ name, email, password, role });
   
   try {
     const token = generateJWT({ name, email, role });
-    console.log(token);
+
+    const hashPassword = crypto.createHash('md5').update(password).digest('hex');
   
-    await User.create({ name, email, password, role });
+    await User.create({ name, email, password: hashPassword, role });
     return token;
   } catch (err) {
     throw customizeError(StatusCodes.BAD_REQUEST, err.message);
