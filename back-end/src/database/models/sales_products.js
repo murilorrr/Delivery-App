0e13c2ecc4bde325f56902495f710e6389748a50
Leandro Liduvino <leandro.augusto460@gmail.com>
2022-02-22 21:53:53 -0300

    fix: corrige erros de sequelize

--- back-end/src/database/models/sales_products.js
+++ back-end/src/database/models/sales_products.js
@@ -5,20 +5,22 @@ module.exports = (sequelize, DataTypes) => {
   {
     timestamps: false,
   });
+
   SalesProducts.associate = (models) => {
     models.Sale.belongsToMany(models.Product, {
-      as: 'sales',
+      as: 'product',
       through: SalesProducts,
       foreignKey: 'sale_id',
-      otherKey: 'product_id',
+      otherKey: 'id', 
     });
+
     models.Product.belongsToMany(models.Sale, {
-      as: 'products',
+      as: 'sale', 
       through: SalesProducts,
       foreignKey: 'product_id',
-      otherKey: 'sale_id',
+      otherKey: 'id',
     });
-  };
+  }
 
   return SalesProducts;
 };
