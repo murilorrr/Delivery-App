842871d6e7725c0b6cea54c177c072f5e4494dc2
Leandro Liduvino <leandro.augusto460@gmail.com>
2022-02-22 22:07:20 -0300

    feat: adiciona rota para listar produto pelo id

---
+++ back-end/src/controllers/products/getProductByIdController.js
@@ -0,0 +1,9 @@
+const { getProductByIdService } = require('../../services/products/getProductByIdService');
+
+const getProductByIdController = async (req, res) => {
+  const { productId } = req.params;
+  const product = await getProductByIdService({ productId });
+  return res.status(200).json(product)
+}
+
+module.exports = { getProductByIdController };
