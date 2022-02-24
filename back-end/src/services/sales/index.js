4f50b4f721a6c1fb86992541f6b62f8bb5cf1d41
Murilo Ribeiro <murilorsv14@gmail.com>
2022-02-24 19:03:09 -0300

    docs: deixa o nome das funçoes mais compreensiveis

--- back-end/src/services/sales/index.js
+++ back-end/src/services/sales/index.js
@@ -1,9 +1,9 @@
-const getAllSalesFromUser = require('./getAllFromUser');
+const getAllSalesByUser = require('./getAllFromUser');
 const getSalesByIdFromUser = require('./getSalesByIdFromUser');
 const createSaleService = require('./createSaleService');
 
 module.exports = {
-  getAll: (userId) => getAllSalesFromUser(userId),
+  getAll: (user) => getAllSalesByUser(user),
   getById: (userId, orderId) => getSalesByIdFromUser(userId, orderId),
   createSale: (saleInfo) => createSaleService(saleInfo),
 };
