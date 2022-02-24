a7109b8bd9b3b03be046b16743064d4810d34590
Murilo Ribeiro <murilorsv14@gmail.com>
2022-02-24 16:08:49 -0300

    refactor: cria contratos para venda

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,15 +1,14 @@
 require('express-async-errors');
 const express = require('express');
-const { sales } = require('../routes/sales');
 const { errorMiddleware } = require('../middlewares');
-const { productsRoute, sallerRoute, loginRoute, userRoute } = require('../routes');
+const { productsRoute, sallerRoute, loginRoute, userRoute, salesRoute } = require('../routes');
 
 const app = express();
 app.use(express.json());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
-app.use(sales);
+app.use(salesRoute);
 app.use(productsRoute);
 app.use(userRoute);
 app.use(sallerRoute);
