3c82401daba506c9c9e92e934e7320d5c1ac8469
Murilo Ribeiro <murilorsv14@gmail.com>
2022-02-24 18:14:09 -0300

    fix: bug erro no contrato do createAnyUser como administrator

--- back-end/src/controllers/admin/creteAnyUser.js
+++ back-end/src/controllers/admin/creteAnyUser.js
@@ -1,12 +1,12 @@
 const { StatusCodes } = require('http-status-codes');
-const { createLikeAdmin } = require('../../services/admin');
+const { createAnyUser } = require('../../services/admin');
 
 const createUser = async (req, res, next) => {
   const user = req.body;
   if (!user.role) user.role = 'costumer';
   const { role: agentRole } = req.user;
   try {
-    const result = await createLikeAdmin(user, agentRole);
+    const result = await createAnyUser(user, agentRole);
     return res.status(StatusCodes.CREATED).json({ token: result });
   } catch (error) {
     next(error);
