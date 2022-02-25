ccfbd3df05ba7c3905364f7b8daba0e6459288cb
Leandro Liduvino <leandro.augusto460@gmail.com>
2022-02-25 10:53:43 -0300

    feat: adiciona token na requisicao quando eles existir

--- front-end/src/fetchs/api.js
+++ front-end/src/fetchs/api.js
@@ -6,4 +6,12 @@ const api = axios.create({
   baseURL,
 });
 
+api.interceptors.request.use(
+  (config) => {
+    const user = localStorage.getItem('user');
+    if (user) config.headers.Authorization = user.token;
+    return config;
+  },
+);
+
 export default api;
