522e3eee5945a9036012f55f99d0c684f4563d47
Leandro Liduvino <leandro.augusto460@gmail.com>
2022-02-25 01:26:45 -0300

    feat: adiciona cors no backend para comunicacao com frontend

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,12 @@
 require('express-async-errors');
 const express = require('express');
+const cors = require('cors');
 const { errorMiddleware } = require('../middlewares');
 const { productsRoute, sellerRoute, loginRoute, userRoute, salesRoute } = require('../routes');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
