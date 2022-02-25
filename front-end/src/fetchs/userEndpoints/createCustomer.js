26b7ebc4cd8874d538d13dab6bb34fa84c9cb1bf
Murilo Ribeiro <81259197+murilorsv14@users.noreply.github.com>
2022-02-25 19:07:29 -0300

    Update createCustomer.js

--- front-end/src/fetchs/userEndpoints/createCustomer.js
+++ front-end/src/fetchs/userEndpoints/createCustomer.js
@@ -4,7 +4,7 @@ async function createUser(user) {
   const result = await api
     .post('/user', user)
     .then((response) => (response.data))
-    .catch((err) => (err.response.data.message));
+    .catch((err) => ({ error: err.response.data.message }));
   return result;
 }
 
