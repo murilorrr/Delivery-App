a8b11ae99ec6996e2de488aff07f38e85d51513a
Murilo Ribeiro <murilorsv14@gmail.com>
2022-02-25 18:05:28 -0300

    feat: adiciona pagina home que redireciona para login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Login, Register } from './pages';
+import { Login, Register, Home } from './pages';
 
 function App() {
   return (
     <Switch>
-      <Route exact path="/" component={ Login } />
+      <Route exact path="/" component={ Home } />
       <Route exact path="/login" component={ Login } />
       <Route exact path="/register" component={ Register } />
       {/* <Route exact path="/" component={  } /> */}
