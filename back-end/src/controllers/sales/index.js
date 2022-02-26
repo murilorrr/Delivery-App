a49da1777e54be270bcc3485adbca6e6689a79d8
Brenojbs <brenojbs@hotmail.com>
2022-02-26 15:26:25 -0300

    feat: Criando endpoint getOrder.

--- back-end/src/controllers/sales/index.js
+++ back-end/src/controllers/sales/index.js
@@ -1,9 +1,11 @@
 const getAllSales = require('./getAllSales');
 const getSalesById = require('./getSalesById');
+const getOrder = require('./getSale');
 const createSaleController = require('./createSaleController');
 
 module.exports = {
   getAllSales,
   getSalesById,
+  getOrder,
   createSale: createSaleController,
 };
