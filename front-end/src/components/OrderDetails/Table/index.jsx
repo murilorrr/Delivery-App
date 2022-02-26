877e15216c963f9c07ead612256ee2dc9b899517
Leandro Liduvino <leandro.augusto460@gmail.com>
2022-02-26 14:34:13 -0300

    feat: adiciona pagina de detalhes do pedido

---
+++ front-end/src/components/OrderDetails/Table/index.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function OrderDetailsTable({ products }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Item</th>
+          <th>Descrição</th>
+          <th>Quantidade</th>
+          <th>Valor unitário</th>
+          <th>Sub-total</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          products && products.map((product, index) => (
+            <tr key={ product.id }>
+              <td
+                data-testid={
+                  `customer_order_details__element-order-table-item-number-${index}`
+                }
+              >
+                { product.id }
+              </td>
+              <td
+                data-testid={
+                  `customer_order_details__element-order-table-name-${index}`
+                }
+              >
+                { product.name }
+              </td>
+              <td
+                data-testid={
+                  `customer_order_details__element-order-table-quantity-${index}`
+                }
+              >
+                { product.quantity }
+              </td>
+              <td
+                data-testid={
+                  `customer_order_details__element-order-table-sub-total-${index}`
+                }
+              >
+                { product.price }
+              </td>
+              <td
+                data-testid={
+                  `customer_order_details__element-order-total-price-${index}`
+                }
+              >
+                { product.price * product.quantity }
+              </td>
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
+  );
+}
+
+OrderDetailsTable.propTypes = {
+  products: PropTypes.arrayOf({}),
+}.isRequired;
+
+export default OrderDetailsTable;
