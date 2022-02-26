c293d00114aa3ebd28209edf21b10f243f1bb215
Murilo Ribeiro <murilorsv14@gmail.com>
2022-02-25 21:03:37 -0300

    feat: adiciona rota back para pegar usuario pelo email

--- back-end/src/routes/userRoute.js
+++ back-end/src/routes/userRoute.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { create, getAll } = require('../controllers/user');
+const { create, getAll, getByEmail } = require('../controllers/user');
 const { authMiddleware } = require('../middlewares');
 
 const router = Router();
@@ -7,5 +7,7 @@ const router = Router();
 router.post('/user', create);
 
 router.get('/user', authMiddleware, getAll);
+
+router.get('/user/:email', authMiddleware, getByEmail);
 
 module.exports = router;
