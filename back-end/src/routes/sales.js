dd7210d765260af075ebf19e5e1830bdea52cbd1
Leandro Liduvino <leandro.augusto460@gmail.com>
2022-02-26 13:17:44 -0300

    fix: adiciona authMiddleware na rota de criacao de venda

--- back-end/src/routes/sales.js
+++ back-end/src/routes/sales.js
@@ -4,7 +4,7 @@ const { createSale, getAllSales } = require('../controllers/sales');
 const sales = Router();
 const { authMiddleware } = require('../middlewares');
 
-sales.post('/customer/sales', createSale);
+sales.post('/customer/sales', authMiddleware, createSale);
 sales.get('/customer/sales', authMiddleware, getAllSales);
 
 module.exports = sales;
