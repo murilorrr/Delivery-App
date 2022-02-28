ddc62efd98f7a65a76c60046e91f16541142cc3d
Brenojbs <brenojbs@hotmail.com>
2022-02-27 21:23:22 -0300

    Feat: Adicionando endpoint getAllSeller.js e chamando no index

--- front-end/src/fetchs/index.js
+++ front-end/src/fetchs/index.js
@@ -5,6 +5,7 @@ export { default as loginUser } from './loginEndpoints/loginUser';
 export { default as getAllUsers } from './userEndpoints/getAllUsers';
 export { default as getAllSalesFromUser } from './sellerEndpoints/getAllSalesFromUser';
 export { default as getSaleByIdFromUser } from './sellerEndpoints/getSaleByIdFromUser';
+export { default as getAllSeller } from './sellerEndpoints/getAllSeller';
 export { default as createAnyUser } from './adminEndpoints/createAnyUser';
 export { default as deleteUser } from './adminEndpoints/deleteUser';
 export { default as getByEmail } from './userEndpoints/getByEmail';
