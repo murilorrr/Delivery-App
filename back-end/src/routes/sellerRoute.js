74ca85c143c5bd0a5464bae18e4f3fbb67101e8d
Leandro Liduvino <leandro.augusto460@gmail.com>
2022-03-03 19:14:37 -0300

    feat: adiciona rota para ter nome e id dos vendedores

--- back-end/src/routes/sellerRoute.js
+++ back-end/src/routes/sellerRoute.js
@@ -1,8 +1,11 @@
 const { Router } = require('express');
 const { getAllSales, getSalesById } = require('../controllers/sales');
+const { getAllSellers } = require('../controllers/sellers');
 const { authMiddleware } = require('../middlewares');
 
 const sellerRoute = Router();
+
+sellerRoute.get('/seller', authMiddleware, getAllSellers);
 
 sellerRoute.get('/seller/orders', authMiddleware, getAllSales);
 
