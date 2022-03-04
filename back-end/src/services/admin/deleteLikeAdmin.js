05c01f4f415a465e6ba40c13afe3a2da7308043f
Murilo Ribeiro <murilorsv14@gmail.com>
2022-03-04 01:02:25 -0300

    fix: tratamente desnecessario

--- back-end/src/services/admin/deleteLikeAdmin.js
+++ back-end/src/services/admin/deleteLikeAdmin.js
@@ -16,12 +16,9 @@ const deleteUser = async (id, agentRole) => {
 
   await validateUserExist(id);
   
-  try {
-    await User.destroy({ where: { id } });
-    return;
-  } catch (err) {
-    throw customizeError(StatusCodes.BAD_REQUEST, err.message);
-  }
+  await User.destroy({ where: { id } });
+  return;
+  
 };
 
 module.exports = deleteUser;
