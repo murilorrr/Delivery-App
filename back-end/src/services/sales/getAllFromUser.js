8ddf365b445268723912be4a7664a86d43c34c24
Murilo Ribeiro <murilorsv14@gmail.com>
2022-03-05 17:47:23 -0300

    fix: requisição para pegar todas as vendas daquele vendedor

--- back-end/src/services/sales/getAllFromUser.js
+++ back-end/src/services/sales/getAllFromUser.js
@@ -3,7 +3,7 @@ const { Sale, User } = require('../../database/models');
 const getAllSalesByUser = async (user) => {
   const actualUser = await User.findOne({ where: { name: user.name } });
 
-  const products = await Sale.findAll({ where: { userId: actualUser.id } });
+  const products = await Sale.findAll({ where: { sellerId: actualUser.id } });
   
   return products;
 };
