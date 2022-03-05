29d953889fb342073c963cf90962d1b4662eb9c6
Leandro Liduvino <leandro.augusto460@gmail.com>
2022-03-05 16:59:16 -0300

    refactor: faz query tanto para users quanto para sellers

--- back-end/src/services/sales/getAllFromUser.js
+++ back-end/src/services/sales/getAllFromUser.js
@@ -1,7 +1,15 @@
+const { Op } = require('sequelize');
 const { Sale } = require('../../database/models');
 
 const getAllSalesByUser = async (user) => {
-  const products = await Sale.findAll({ where: { sellerId: user.id } });
+  const products = await Sale.findAll({
+    where: {
+      [Op.or]: [
+        { sellerId: user.id },
+        { userId: user.id },
+      ],
+    },
+  });
   
   return products;
 };
