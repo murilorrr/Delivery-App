290cc3dcac2269a4d2b4538e365cd4a7b4a01f4f
Murilo Ribeiro <murilorsv14@gmail.com>
2022-03-10 17:38:40 -0300

    commentando console.log do socket na aplicacao

--- back-end/src/sockets/changeOrderStatus.js
+++ back-end/src/sockets/changeOrderStatus.js
@@ -1,19 +1,19 @@
 const updateSaleStatus = require('../services/sales/updateSaleStatus');
 
-let count = 0;
+// let count = 0;
 
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    count += 1;
-    console.log(`conectado ${count} vezes`);
-    console.log(`com id: ${socket.id || io.id}`);
+    // count += 1;
+    // console.log(`conectado ${count} vezes`);
+    // console.log(`com id: ${socket.id || io.id}`);
 
     socket.on('updateStatus', async ({ saleId, status }) => {
       try {
         await updateSaleStatus({ saleId, status });
         io.emit('statusUpdated', { saleId, status });
       } catch (error) {
-        console.log(error);
+        // console.log(error);
         io.emit('error', 'unable to update status');
       }
     });
